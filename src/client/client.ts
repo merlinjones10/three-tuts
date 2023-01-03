7a408ab7d9645ccfcbce0fd42a383e6c89060dad
merlinjones10 <78985319+merlinjones10@users.noreply.github.com>
2023-01-03 19:55:53 +0000

    revert to boilerplate

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,64 +1,38 @@
 import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
 const scene = new THREE.Scene()
-const scene2 = new THREE.Scene()
 
-const camera = new THREE.PerspectiveCamera(75, 1, 0.1, 10)
-
-const camera2 = new THREE.OrthographicCamera(-2, 2, 2, -2)
-const camera3 = new THREE.OrthographicCamera(-2, 2, 2, -2)
-const camera4 = new THREE.OrthographicCamera(-2, 2, 2, -2)
-
+const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+)
 camera.position.z = 2
 
-camera2.position.y = 2
-camera2.lookAt(new THREE.Vector3()) // same as 0,0,0,
+const renderer = new THREE.WebGLRenderer()
+renderer.setSize(window.innerWidth, window.innerHeight)
+document.body.appendChild(renderer.domElement)
 
-camera3.position.x = -2
-camera3.lookAt(new THREE.Vector3(0, 0, 0)) // same as 0,0,0,
+new OrbitControls(camera, renderer.domElement)
 
-camera4.position.z = 2
-
-const canvas1 = document.getElementById('c1') as HTMLCanvasElement
-const canvas2 = document.getElementById('c2') as HTMLCanvasElement
-const canvas3 = document.getElementById('c3') as HTMLCanvasElement
-const canvas4 = document.getElementById('c4') as HTMLCanvasElement
-
-const renderer1 = new THREE.WebGLRenderer({ canvas: canvas1 })
-renderer1.setSize(200, 200)
-const renderer2 = new THREE.WebGLRenderer({ canvas: canvas2 })
-renderer2.setSize(200, 200)
-const renderer3 = new THREE.WebGLRenderer({ canvas: canvas3 })
-renderer3.setSize(200, 200)
-const renderer4 = new THREE.WebGLRenderer({ canvas: canvas4 })
-renderer4.setSize(200, 200)
-
-// document.body.appendChild(renderer1.domElement)
-// document.body.appendChild(renderer2.domElement)
-
-// new OrbitControls(camera, renderer.domElement)
-
-const geometry = new THREE.TorusKnotGeometry() // THREE.BoxGeometry
+const geometry = new THREE.BoxGeometry()
 const material = new THREE.MeshBasicMaterial({
     color: 0x00ff00,
     wireframe: true,
 })
 
 const cube = new THREE.Mesh(geometry, material)
-cube.scale.x = 0.5
-cube.scale.y = 0.5
-cube.scale.z = 0.5
 scene.add(cube)
 
-const cube2 = new THREE.Mesh(geometry, material)
-scene2.add(cube2)
-// window.addEventListener('resize', onWindowResize, false)
-// function onWindowResize() {
-//     camera.aspect = window.innerWidth / window.innerHeight
-//     camera.updateProjectionMatrix()
-//     renderer.setSize(window.innerWidth, window.innerHeight)
-//     render()
-// }
+window.addEventListener('resize', onWindowResize, false)
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight
+    camera.updateProjectionMatrix()
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    render()
+}
 
 function animate() {
     requestAnimationFrame(animate)
@@ -70,10 +44,7 @@ function animate() {
 }
 
 function render() {
-    renderer1.render(scene, camera)
-    renderer2.render(scene2, camera2)
-    renderer3.render(scene, camera3)
-    renderer4.render(scene, camera4)
+    renderer.render(scene, camera)
 }
 
 animate()
